4c1005b25ec376d48aa30f1c8da4cf5ffeaa201f
dave <dave@dave-THINK.localdomain>
2013-09-05 11:06:14 +0200

    test

---
+++ src/springapp/web/HelloController.java
@@ -0,0 +1,17 @@
+package springapp.web;
+
+import java.io.IOException;
+
+public class HelloController implements Controller {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Returning hello view");
+
+        return new ModelAndView("hello.jsp");
+    }
+
+}
